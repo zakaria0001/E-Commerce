fed3ebea40f051b3e8f74423b5350af8f74d75bc
Zakaria <zakarianabil68@gmail.com>
2020-10-07 01:10:21 +0100

    commit changes

---
+++ index.js
@@ -0,0 +1,17 @@
+//VAlidation Code for inputs 
+
+
+var email  =   document.forms['form']["email"];
+var password  =   document.forms['form']["password"];
+
+
+var email_error   =  document.getElementById('email_error');
+var pass_error  =   document.getElementById('pass_error');
+ 
+
+function validated() {
+    
+
+    
+      
+}
---
+++ main.js
@@ -0,0 +1,47 @@
+const inputs =index.querySelectorAll(".input");
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+var objPeople = [
+	{ // Object @ 0 index
+		username: "sam",
+		password: "codify"
+	},
+	{ // Object @ 1 index
+		username: "matt",
+		password: "academy"
+	},
+	{ // Object @ 2 index
+		username: "chris",
+		password: "forever"
+	}
+
+]
+
+function getInfo() {
+	var username = inputs.getElementsByClassName('username').value
+	var password = inputs.getElementsByClassName('password').value
+
+	for(var i = 0; i < objPeople.length; i++) {
+		// check is user input matches username and password of a current index of the objPeople array
+		if(username == objPeople[i].username && password == objPeople[i].password) {
+location.replace('/.html')
+			return
+		}
+	}
+}
